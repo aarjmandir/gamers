fc16fc9fa8247286009a3c0eb843a11412a37f48
Abouzar Arjmand <aarjmand@gmail.com>
2023-08-24 18:35:24 +0330

    Build A Responsive Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 import GameGrid from "./components/GameGrid";
 
 function App() {
-  return <GameGrid />;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg="gold">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="dodgerblue">
+        <GameGrid />
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
